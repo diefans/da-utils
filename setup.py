a037c7c544a858413fc2757e7aaa686d65fa9bac
diefans <oliver@digitalarchitekt.de>
2013-10-29 14:35:23 +0100

    put hierarchy into da.utils

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="da.utils",
     version="0.0.1",
-    namespace_packages=['da', 'da.utils'],
+    namespace_packages=['da'],
     packages=find_packages('src'),
     package_dir={
         '': 'src',
